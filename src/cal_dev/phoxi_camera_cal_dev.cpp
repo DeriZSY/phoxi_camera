42b6a32a211a519cbf58a7b6a8b04734964474a9
bionicdl_labtop <2013207520@qq.com>
2018-09-28 15:09:48 +0800

    macro definition for coloring text added; code refactored

--- src/cal_dev/phoxi_camera_cal_dev.cpp
+++ src/cal_dev/phoxi_camera_cal_dev.cpp
@@ -12,40 +12,49 @@ void pcl_call_back(const sensor_msgs::PointCloud2::ConstPtr& pcl_map)
     ROS_INFO("Point cloud map received in callback function!!!");
 }
 
+bool phoxi_connect(const string& cam_id)
+{
+    ros::NodeHandle n;
+    ros::ServiceClient ConnectCamera_client = n.serviceClient<phoxi_camera::ConnectCamera>("phoxi_camera/connect_camera");
+    phoxi_camera::ConnectCamera ConnectCamera_srv;
+    ConnectCamera_srv.request.name = cam_id;
+    if (ConnectCamera_client.call(ConnectCamera_srv)) { ROS_INFO("Camera Conncted"); }
+    else 
+    {
+        ROS_ERROR("Cammera Not Connected");
+        cout << RD("Filed with Message:") << ConnectCamera_srv.response.message << endl;
+    }
+    return ConnectCamera_srv.response.success;
+}
+
+bool get_frame(const int id)
+{
+    ros::NodeHandle n;
+    ros::ServiceClient GetFrame_client = n.serviceClient<phoxi_camera::GetFrame>("phoxi_camera/get_frame");
+    phoxi_camera::GetFrame GetFrame_srv;
+    GetFrame_srv.request.in = id;
+    if (GetFrame_client.call(GetFrame_srv)) { ROS_INFO("Get Frame Success");}
+    else                       
+    { 
+        ROS_ERROR("Get Frame Failed"); 
+        cout << RD(Failed With Message:) << GetFrame_srv.response.message << endl;
+    }
+    return GetFrame_srv.response.success;
+}
+
 int main (int argc, char** argv)
 {
     ros::init(argc, argv, "phoxi_cal_dev");
     ros::NodeHandle n;
     // Connect to the camera on PhoxiControler
-    ros::ServiceClient ConnectCamera_client = n.serviceClient<phoxi_camera::ConnectCamera>("phoxi_camera/connect_camera");
-    phoxi_camera::ConnectCamera ConnectCamera_srv;
-    ConnectCamera_srv.request.name = "1711004";
-    ConnectCamera_client.call(ConnectCamera_srv);
-    if (ConnectCamera_srv.response.success)
-        ROS_INFO("Camera Conncted");
-    else 
+    bool connectCamera_success = phoxi_connect("1711004");
+    bool getFrame_success = get_frame(-1);
+    cout << GR(Color Test)<<endl;
+    if (connectCamera_success && getFrame_success)
     {
-        cout << ConnectCamera_srv.response.message << endl;
-        ROS_ERROR("Cammera Not Connecting");
-    }
-    // Obtain a frame from the camera
-    ros::ServiceClient GetFrame_client = n.serviceClient<phoxi_camera::GetFrame>("phoxi_camera/get_frame");
-    phoxi_camera::GetFrame GetFrame_srv;
-    GetFrame_srv.request.in = -1;
-    if (GetFrame_client.call(GetFrame_srv))
-    {
-        ROS_INFO("Get Image");
-        cout << GetFrame_srv.response.message << endl;
         ros::Subscriber sub_depthMap = n.subscribe("/phoxi_camera/depth_map",1000,depth_im_call_back);
         ros::Subscriber sub_pointCloud = n.subscribe("/phoxi_camera/pointcloud", 1000, pcl_call_back);
         ros::spin();
     }
-    else
-    {
-        ROS_INFO("Failed");
-        return -1;
-    }
-    
-
     return 0;
 } 
