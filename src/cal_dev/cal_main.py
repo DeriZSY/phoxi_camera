c4b75c9c8a224a3eb252405f53dca5f7e2c151ef
bionicdl_labtop <2013207520@qq.com>
2018-10-10 15:19:04 +0800

    main program finished

--- src/cal_dev/cal_main.py
+++ src/cal_dev/cal_main.py
@@ -11,17 +11,29 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import tf 
 import os 
+import random
 
 NUM_ATTEMPS = 1
 PLANNER_NAME = "RRTConnect"
-MAX_PLAN_TIMES = 5
+MAX_PLAN_TIMES = 2
 
 # FIXME: Unfinished part
-class pose_bound:
-  def __init__(self, translation, rotation):
-    pass
+def get_bound(x,y,z):
+  return tf.transformations.quaternion_from_euler(x, y, z, axes='sxyz')
 
-    
+def copy_pose(original_pose):
+  new_pose = geometry_msgs.msg.Pose()
+  new_pose.position.x = original_pose.position.x
+  new_pose.position.y = original_pose.position.y
+  new_pose.position.z = original_pose.position.z
+
+  new_pose.orientation.x = original_pose.orientation.x
+  new_pose.orientation.y = original_pose.orientation.y
+  new_pose.orientation.z = original_pose.orientation.z
+  new_pose.orientation.w = original_pose.orientation.w
+
+  return new_pose
+
 # TODO
 def get_path_all_around(current_pose):
   ''' 
@@ -35,7 +47,7 @@ def get_path_all_around(current_pose):
   pass
 
 # FIXME: Unfinished codes
-def get_path_zigzag(current_pose):
+def get_path_random(current_pose):
   ''' 
   Get a path that moving slightly around current position in a zig zag manner
 
@@ -47,10 +59,103 @@ def get_path_zigzag(current_pose):
   # bound = pose_bound(x, y, z, yaw, pitch, roll)# create pose bound
   path = []
   new_pose = current_pose
-  new_pose.position.z = current_pose.position.z + 0.03
-  path.append(new_pose)
+  for i in range(4):
+
+    if (random.random() > 0.3):
+      new_pose.position.x += 0.03 * random.random()
+
+    if (random.random() > 0.4):
+      new_pose.position.y += 0.03 * random.random()
+
+    new_pose.position.z +=  0.03 * random.random()
+      
+    path.append(new_pose)
   return path
 
+# FIXME: Unfinished codes
+def get_path_zigzag(current_pose):
+  ''' 
+  Get a path that moving slightly around current position in a zig zag manner
+
+  Target Representation:
+      geometry_msgs -> Pose.msg:
+        Point position (float64 x y z)
+        Quaternion orientation (float64 w+xi+yj+zk)
+  '''
+  # bound = pose_bound(x, y, z, yaw, pitch, roll)# create pose bound
+  path = []
+  new_pose = copy_pose(current_pose)
+
+  new_pose.position.x -= 0.012
+  new_pose.position.y -= 0.012
+  new_pose.position.z -= 0.012
+  path.append(new_pose)
+
+  new_pose = copy_pose(current_pose)
+  new_pose.position.x += 0.024
+  new_pose.position.y += 0.024
+  new_pose.position.z -= 0.012
+  path.append(new_pose)
+
+  new_pose = copy_pose(current_pose)
+  new_pose.position.x -= 0.024
+  new_pose.position.y -= 0.024
+  new_pose.position.z -= 0.012
+  path.append(new_pose)
+
+  new_pose = copy_pose(current_pose)
+  new_pose.position.x += 0.024
+  new_pose.position.y += 0.024
+  new_pose.position.z -= 0.012
+  path.append(new_pose)
+  
+  return path
+
+def get_path_test(current_pose):
+  new_pose = copy_pose(current_pose)
+  path = []
+  new_pose.position.y +=  0.03
+  path.append(new_pose)
+
+  new_pose = copy_pose(current_pose)
+  new_pose.position.z += 0.06
+  path.append(new_pose)
+  # new_pose.position.z += 0.02
+  # path.append(new_pose)
+  return path 
+
+def plan_execute(target, index):
+  plan_list = []
+  print(target)
+  group.set_start_state_to_current_state()
+  group.clear_pose_targets()
+  group.set_pose_target(target)
+  for j in range(MAX_PLAN_TIMES):
+    plan = group.plan()
+    if len(plan.joint_trajectory.points) == 0:
+      continue 
+    plan_list.append(plan)
+    rospy.sleep(0.5)
+  print(group.get_current_pose().pose)
+  if len(plan_list) == 0:
+    print("Fail to get any plan")
+    return
+
+
+  group.execute(plan_list[0])
+  rospy.sleep(3)
+  im_name = "im" + str(index)
+  status = os.system('sh /home/bionicdl/catkin_ws/src/phoxi_camera/image_cap.sh %s'%im_name)
+  # print(status)
+
+def write_pose2data(file_name, pose, index):
+  f = open(file_name, 'a')
+  f.write('index:%d\n'%index)
+  f.write('position:\nx:%f\ny:%f\nz:%f\norientation:\nw:%f\nx:%f\ny:%f\nz:%f\n\n'
+  %(pose.position.x,pose.position.y, pose.position.z, 
+    pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z ))
+  f.write('\n')
+  f.close()
 
 
 # initialize moveit_commander and rospy node
@@ -63,7 +168,7 @@ robot = moveit_commander.RobotCommander()
 # instantiate a Planning SceneInterface object
 scene = moveit_commander.PlanningSceneInterface()
 # Instantiate a Move Group Commander object
-group = moveit_commander.MoveGroupCommander("Arm")
+group = moveit_commander.MoveGroupCommander("manipulator_i5")
 # publish trajectory to RViz
 display_trajectory_publisher = rospy.Publisher(
                                       '/move_group/display_planned_path',
@@ -76,28 +181,18 @@ group.set_planner_id(PLANNER_NAME+'kConfig1')
 group.set_num_planning_attempts(1)
 group.set_planning_time(5)
 
-plan_zigzag = []
+path_zigzag = get_path_zigzag(group.get_current_pose().pose)
+# path_zigzag = get_path_test(group.get_current_pose().pose)
 
-path_zigzag = get_path_zigzag(group.get_current_joint_values())
+file_name = "/home/bionicdl/calibration_images/data.txt"
+os.system('touch %s'%file_name)
 
-# Rough Calibration
-for path_target in path_zigzag:
-  group.set_pose_target(path_target)
-  for j in range(MAX_PLAN_TIMES):
-    plan = group.plan()
-    if len(plan.joint_trajectory.points) == 0:
-      continue 
-    plan_list.append(plan)
-    rospy.sleep(0.5)
+t = 1
+for target in path_zigzag:
+  plan_execute(target,t)
+  write_pose2data(file_name, target, t)
+  t = t+1
 
-  if len(plans_place) == 0:
-    print("Fail to get any plan")
-    break
-
-  group.execute(plans_place[0])
-  status = os.system('sh /home/bionicdl/catkin_ws/src/phoxi_camera/image_cap.sh')
-  rospy.sleep(10)
-  print(status)
     
 
 #print('sucess rate for pick: %s and place: %s'%(len(plans_pick),len(plans_place)))
